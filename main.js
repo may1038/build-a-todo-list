c663719e387780f9db0c14fe8d1526f17271751f
Tyler May <tylermay@Tylers-MacBook-Pro.local>
2017-08-16 20:04:45 -0500

    Finished todo list

--- main.js
+++ main.js
@@ -9,14 +9,28 @@ app.use(expressValidator())
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get("/", function(req, res) {
-  res.render("index", {
-  })
+const todos = [];
+
+const completed = [];
+
+app.get("/", function (req, res) {
+  res.render('index', {
+    todos: todos,
+    completed: completed
+  });
+});
+
+app.post("/", function (req, res) {
+  todos.push(req.body.todo);
+  res.redirect('/');
 })
 
-
-
-
+app.post("/completed", function (req, res) {
+  const remove = req.body.button
+  todos.splice(todos.indexOf(remove), 1)
+  completed.push(remove);
+  res.redirect('/');
+})
 
 
 app.listen(3000, function() {
